0ec500b42089e74d941561b4b704dafada692d36
streambinder <posta@davidepucci.it>
2023-02-05 19:28:19 +0100

    build(template): lint

--- .make/template.py
+++ .make/template.py
@@ -1,74 +1,76 @@
 #!/usr/bin/env python3
 
 import glob
-import jinja2
 import os
-import yaml
-
-from jinja2 import Template
 from distutils.dir_util import copy_tree
 
+import jinja2
+import yaml
+
 j_envs = {
-    'html': jinja2.Environment(
-        block_start_string='<jblock',
-        block_end_string='/>',
-        variable_start_string='<jvar',
-        variable_end_string='/>',
-        comment_start_string='<!--',
-        comment_end_string='-->',
+    "html": jinja2.Environment(
+        block_start_string="<jblock",
+        block_end_string="/>",
+        variable_start_string="<jvar",
+        variable_end_string="/>",
+        comment_start_string="<!--",
+        comment_end_string="-->",
         trim_blocks=True,
         autoescape=False,
-        loader=jinja2.FileSystemLoader(os.path.abspath('.'))
+        loader=jinja2.FileSystemLoader(os.path.abspath(".")),
     ),
-    'tex': jinja2.Environment(
-        block_start_string='\\jblock{',
-        block_end_string='}',
-        variable_start_string='\\jvar{',
-        variable_end_string='}',
-        comment_start_string='\\#{',
-        comment_end_string='}',
-        line_statement_prefix='%%',
-        line_comment_prefix='%#',
+    "tex": jinja2.Environment(
+        block_start_string="\\jblock{",
+        block_end_string="}",
+        variable_start_string="\\jvar{",
+        variable_end_string="}",
+        comment_start_string="\\#{",
+        comment_end_string="}",
+        line_statement_prefix="%%",
+        line_comment_prefix="%#",
         trim_blocks=True,
         autoescape=False,
-        loader=jinja2.FileSystemLoader(os.path.abspath('.'))
+        loader=jinja2.FileSystemLoader(os.path.abspath(".")),
     ),
-    'txt': jinja2.Environment(
-        block_start_string='{%',
-        block_end_string='%}',
-        variable_start_string='{{',
-        variable_end_string='}}',
-        comment_start_string='{#',
-        comment_end_string='#}',
+    "txt": jinja2.Environment(
+        block_start_string="{%",
+        block_end_string="%}",
+        variable_start_string="{{",
+        variable_end_string="}}",
+        comment_start_string="{#",
+        comment_end_string="#}",
         trim_blocks=True,
         autoescape=False,
-        loader=jinja2.FileSystemLoader(os.path.abspath('.'))
-    )
+        loader=jinja2.FileSystemLoader(os.path.abspath(".")),
+    ),
 }
 
 
-for config in glob.glob('src/langs/*.yml'):
-    lang = config.split(os.path.sep)[-1].split('.yml')[0]
+for config in glob.glob("src/langs/*.yml"):
+    lang = config.split(os.path.sep)[-1].split(".yml")[0]
     config_lang = None
-    with open(config, 'r') as config_fd:
+    with open(config, "r") as config_fd:
         try:
             config_lang = yaml.safe_load(config_fd)
         except yaml.YAMLError as e:
             print(e)
 
     if config_lang is None:
-        print('unable to parse {}'.format(config))
+        print("unable to parse {}".format(config))
         continue
 
-    for template in glob.glob('src/templates/*.j2'):
-        t_format = template.split('.')[-2]
+    for template in glob.glob("src/templates/*.j2"):
+        t_format = template.split(".")[-2]
         if t_format not in j_envs:
-            print('template format {} not supported'.format(t_format))
+            print("template format {} not supported".format(t_format))
             continue
 
-        t_name = template.split(os.path.sep)[-1].split('.{}'.format(t_format))[0]
+        t_name = template.split(os.path.sep)[-1].split(".{}".format(t_format))[0]
         j_envs[t_format].get_template(template).stream(config_lang).dump(
-            '{}/templates/{}_{}.{}'.format(os.environ['BUILD_DIR'],  t_name, lang, t_format))
+            "{}/templates/{}_{}.{}".format(
+                os.environ["BUILD_DIR"], t_name, lang, t_format
+            )
+        )
 
-    if os.path.isdir('src/assets'):
-        copy_tree('src/assets', '{}'.format(os.environ['BUILD_DIR']))
+    if os.path.isdir("src/assets"):
+        copy_tree("src/assets", "{}".format(os.environ["BUILD_DIR"]))
