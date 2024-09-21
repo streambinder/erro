694e3713ec03a4133b9226c893fd060487d0fe7e
streambinder <posta@davidepucci.it>
2024-09-21 20:28:32 +0200

    build(make): minor adjustments

--- .make/render.sh
+++ .make/render.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-cd "${BUILD_DIR}" || exit 1
+set -euo pipefail
+
+cd "${BUILD_DIR}"
 
 mv -f templates/*{html,txt} ./
 
 for template_tex in templates/*.tex; do
-	pdflatex -synctex=1 -interaction=nonstopmode \
-		"${template_tex}" >/dev/null 2>&1
+	pdflatex -synctex=1 -interaction=nonstopmode "${template_tex}"
 done
 
 find . -maxdepth 1 -type f -not -name '*.pdf' -a -not -name '*.html' -a -not -name '*.txt' -delete
